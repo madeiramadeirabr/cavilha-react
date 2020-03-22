0012bafd9468a164d56c199ef7fabc2738d122af
William Novak <williamnvk@gmail.com>
2020-03-22 18:33:37 -0300

    update button component and update prettier and eslint

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,30 @@
-import React from 'react';
+import React, { useState, MouseEvent, useRef } from 'react';
+import { Button } from './components/button/index';
 
 export default function App() {
-  return <h1>Hello world</h1>
+  const [loading, setLoading] = useState(false);
+  const buttonRef = useRef();
+
+  function handleClick(event: MouseEvent<HTMLButtonElement>): void {
+    event.preventDefault();
+    setLoading(!loading);
+  }
+
+  return (
+    <h1>
+      <Button
+        id="button-id"
+        ref={buttonRef}
+        modifiers={{
+          type: 'button--is-primary',
+          variants: ['button--is-large', 'button--is-outline'],
+        }}
+        onClick={handleClick}
+        loading={loading}
+        loadingText="aguarde"
+      >
+        Test me
+      </Button>
+    </h1>
+  );
 }
--- src/components/button/Button.tsx
+++ src/components/button/Button.tsx
@@ -1,5 +1,76 @@
-import * as React from "react"
+import React, {
+  ElementType,
+  ComponentPropsWithRef,
+  forwardRef,
+  Ref,
+} from 'react';
 
-export function Button() {
-  return <button>William</button>
-}
+/**
+ * Util
+ */
+type MergeElementProps<T extends ElementType, P extends object = {}> = Omit<
+  ComponentPropsWithRef<T>,
+  keyof P
+> &
+  P;
+
+/**
+ * Props
+ */
+type ButtonTypeModifiers = 'button--is-primary' | 'button--is-secondary';
+type ButtonVariantModifiers =
+  | 'button--is-outline'
+  | 'button--is-small'
+  | 'button--is-large'
+  | 'button--is-extra-large'
+  | 'button--is-disabled';
+type ButtonHelperModifiers = 'helper--is-full-width';
+
+type ButtonModifiers = {
+  type: ButtonTypeModifiers;
+  variants?: Array<ButtonVariantModifiers>;
+  helpers?: Array<ButtonHelperModifiers>;
+};
+
+type ButtonProps = MergeElementProps<
+  'button',
+  {
+    id: string;
+    modifiers: ButtonModifiers;
+    loading?: boolean;
+    loadingText?: string;
+  }
+>;
+
+/**
+ * Component
+ */
+const Button = forwardRef(
+  (
+    { children, modifiers, loading, loadingText, ...props }: ButtonProps,
+    ref: Ref<HTMLButtonElement>
+  ) => {
+    props.ref = ref;
+
+    const classes = [
+      'button',
+      modifiers.type,
+      [modifiers.variants ? modifiers.variants.join(' ') : ''],
+      [modifiers.helpers ? modifiers.helpers.join(' ') : ''],
+    ]
+      .filter(Boolean)
+      .join(' ')
+      .trim();
+
+    if (loading) {
+      props.disabled = true;
+    }
+    return (
+      <button type="button" className={classes} {...props}>
+        {loading ? loadingText : children}
+      </button>
+    );
+  }
+);
+
+export { Button };
